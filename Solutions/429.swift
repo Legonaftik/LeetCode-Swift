2b90be80a2fe744056ca5e18daddd7cf867133c8
Vladimir Pavlov <legonaftik@gmail.com>
2021-08-07 10:00:38 +0300

    429

---
+++ Solutions/429.swift
@@ -0,0 +1,76 @@
+class Solution {
+    func levelOrder(_ root: Node?) -> [[Int]] {
+        guard let root = root else { return [] }
+        var answer: [[Int]] = []
+        var queue = Queue<Node>()
+        queue.enqueue(root)
+        while !queue.isEmpty {
+            var level: [Int] = []
+            for _ in 0..<queue.count {
+                let node = queue.dequeue()!
+                level.append(node.val)
+                node.children.forEach { queue.enqueue($0) }
+            }
+            answer.append(level)
+        }
+        return answer
+    }
+}
+
+
+/*
+  First-in first-out queue (FIFO)
+
+  New elements are added to the end of the queue. Dequeuing pulls elements from
+  the front of the queue.
+
+  Enqueuing and dequeuing are O(1) operations.
+*/
+public struct Queue<T> {
+  fileprivate var array = [T?]()
+  fileprivate var head = 0
+
+  public var isEmpty: Bool {
+    return count == 0
+  }
+
+  public var count: Int {
+    return array.count - head
+  }
+
+  public mutating func enqueue(_ element: T) {
+    array.append(element)
+  }
+
+  public mutating func dequeue() -> T? {
+    guard let element = array[guarded: head] else { return nil }
+
+    array[head] = nil
+    head += 1
+
+    let percentage = Double(head)/Double(array.count)
+    if array.count > 50 && percentage > 0.25 {
+      array.removeFirst(head)
+      head = 0
+    }
+
+    return element
+  }
+
+  public var front: T? {
+    if isEmpty {
+      return nil
+    } else {
+      return array[head]
+    }
+  }
+}
+
+extension Array {
+    subscript(guarded idx: Int) -> Element? {
+        guard (startIndex..<endIndex).contains(idx) else {
+            return nil
+        }
+        return self[idx]
+    }
+}
