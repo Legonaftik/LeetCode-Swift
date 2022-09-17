b3dd492aaaaa64502c2f094e3a457210e0b731e6
Vladimir Pavlov <legonaftik@gmail.com>
2022-09-17 20:00:14 +0100

    2

--- Solutions/2.swift
+++ Solutions/2.swift
@@ -1,40 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init() { self.val = 0; self.next = nil; }
- *     public init(_ val: Int) { self.val = val; self.next = nil; }
- *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
- * }
- */
 class Solution {
-    func addTwoNumbers(
-        _ l1: ListNode?,
-        _ l2: ListNode?
-    ) -> ListNode? {
-        var pseudoHead = ListNode(-1)
-        var previousNode: ListNode? = pseudoHead
-        var node1 = l1
-        var node2 = l2
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let pseudoHead = ListNode()
+        var previous = pseudoHead
         var carry = 0
+        var l1 = l1
+        var l2 = l2
 
-        while node1 != nil || node2 != nil {
-            let val1 = node1?.val ?? 0
-            let val2 = node2?.val ?? 0
-            let sum = val1 + val2 + carry
+        while l1 != nil || l2 != nil {
+            let sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry
             carry = sum / 10
-            let newDigit = sum % 10
-            let newNode = ListNode(newDigit)
-            previousNode?.next = newNode
-            previousNode = newNode
-
-            node1 = node1?.next
-            node2 = node2?.next
+            let newNode = ListNode(sum % 10)
+            previous.next = newNode
+            previous = newNode
+            l1 = l1?.next
+            l2 = l2?.next
         }
 
         if carry > 0 {
-            previousNode?.next = ListNode(carry)
+            previous.next = ListNode(carry)
         }
         return pseudoHead.next
     }
