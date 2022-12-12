9bb118a02821f9130d7487d715560a772c29660c
Vladimir Pavlov <legonaftik@gmail.com>
2022-12-12 23:31:23 +0000

    63

--- Solutions/63.swift
+++ Solutions/63.swift
@@ -1,15 +1,27 @@
 class Solution {
     func uniquePathsWithObstacles(_ obstacleGrid: [[Int]]) -> Int {
+        guard obstacleGrid.first?.first == 0 else {
+            return 0
+        }
+
         var dp: [[Int]] = Array(
             repeating: Array(repeating: 0, count: obstacleGrid[0].count),
             count: obstacleGrid.count
         )
-        dp[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0
+        dp[0][0] = 1
         for row in 1..<dp.endIndex {
-            dp[row][0] = obstacleGrid[row][0] == 0 ? dp[row-1][0] : 0
+            if obstacleGrid[row][0] == 0 {
+                dp[row][0] = 1
+            } else {
+                break
+            }
         }
         for column in 1..<dp[0].endIndex {
-            dp[0][column] = obstacleGrid[0][column] == 0 ? dp[0][column-1] : 0
+            if obstacleGrid[0][column] == 0 {
+                dp[0][column] = 1
+            } else {
+                break
+            }
         }
 
         for row in 1..<dp.endIndex {
