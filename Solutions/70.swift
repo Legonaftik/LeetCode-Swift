e776cfc272989d6d1d18d0c040197c6503de10e2
Vladimir Pavlov <legonaftik@gmail.com>
2022-12-13 19:34:42 +0000

    70

--- Solutions/70.swift
+++ Solutions/70.swift
@@ -1,21 +1,13 @@
 class Solution {
-
-    private var memo: [Int: Int] = [:]
+    private var memo: [Int: Int] = [0: 1, 1: 1]
 
     func climbStairs(_ n: Int) -> Int {
-        switch n {
-        case 1:
-            return 1
-        case 2:
-            return 2
-        default:
-            if let cachedValue = memo[n] {
-                return cachedValue
-            } else {
-                let answer = climbStairs(n-1) + climbStairs(n-2)
-                memo[n] = answer
-                return answer
-            }
+        if let cachedValue = memo[n] {
+            return cachedValue
+        } else {
+            let output = climbStairs(n-1) + climbStairs(n-2)
+            memo[n] = output
+            return output
         }
     }
 }
