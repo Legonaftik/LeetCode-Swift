a0b5fbf5d718af580d37b03f91d8f15493d443ee
Vova Pavlov <legonaftik@gmail.com>
2023-01-04 00:04:51 +0000

    129

--- Solutions/129.swift
+++ Solutions/129.swift
@@ -1,33 +1,14 @@
 class Solution {
     func sumNumbers(_ root: TreeNode?) -> Int {
-        if let root = root {
-            return subOfSubtree(node: root, interimString: String(root.val))
-        } else {
-            return 0
+        func sumNumbers(_ root: TreeNode?, pathSum: Int) -> Int {
+            guard let root = root else { return 0 }
+            let pathSum = pathSum * 10 + root.val
+            if root.left == nil && root.right == nil {
+                return pathSum
+            } else {
+                return sumNumbers(root.left, pathSum: pathSum) + sumNumbers(root.right, pathSum: pathSum)
+            }
         }
-    }
-
-    private func subOfSubtree(
-        node: TreeNode,
-        interimString: String
-    ) -> Int {
-        var sumOfChildren = 0
-        if let leftChild = node.left {
-            sumOfChildren += subOfSubtree(
-                node: leftChild,
-                interimString: interimString + String(leftChild.val)
-            )
-        }
-        if let rightChild = node.right {
-            sumOfChildren += subOfSubtree(
-                node: rightChild,
-                interimString: interimString + String(rightChild.val)
-            )
-        }
-        if node.left == nil && node.right == nil {
-            return Int(interimString) ?? 0
-        } else {
-            return sumOfChildren   
-        }
+        return sumNumbers(root, pathSum: 0)
     }
 }
